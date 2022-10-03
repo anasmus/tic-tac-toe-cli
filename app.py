06e7d0776e38f49651406e84dd355c5c6007d059
Anas Mustafa <anasmtfa@gmail.com>
2022-10-03 15:28:07 +0500

    Add Logic to Check for Winner

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ from random import choice as random_choice
 WIDTH = os.get_terminal_size().columns
 def main():
   display_guide()
-  available_moves = [x for x in range(1,10)]
+  available_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   movelist = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   display_board(movelist)
   while True:
     user_input(available_moves, movelist)
-    update_board(movelist)
     if not available_moves:
-      break
+      declare_tie(movelist)
     bot_input(available_moves, movelist)
-    update_board(movelist)
 
 def user_input(available_moves, movelist):
   print("\n")
@@ -23,19 +21,47 @@ def user_input(available_moves, movelist):
     except:
       print("Please Enter a Number corresponding to Your Move")
       move = 0
-  available_moves.remove(move)
-  movelist[move - 1] = "X"
+  update_status(available_moves, movelist, move, "X")
 
 def bot_input(available_moves, movelist):
   move = random_choice(available_moves)
-  available_moves.remove(move)
-  movelist[move - 1] = "O"
   print(f"Bot Played {move}")
-  input("Press any key to start the game...")
+  input("Press any key to continue...")
+  update_status(available_moves, movelist, move, "O")
+
+def update_status(available_moves, movelist, current_move, symbol):
+  available_moves.remove(current_move)
+  movelist[current_move - 1] = symbol
+  update_board(movelist)
+  check_game(movelist, symbol)
 
 def update_board(movelist):
   cls()
   display_board(movelist)
+
+def check_game(movelist, symbol):
+  if movelist.count(symbol) < 3:
+    return 
+  else:
+    win = False
+    # checking for vertical and horizontal lines
+    for i in range(3):
+      if ((movelist[i] == movelist[i + 3] == movelist[i + 6]) or 
+          (movelist[3 * i] == movelist[3 * i + 1] == movelist[3 * i + 2])):
+        win = True
+    # checking for diagonal lines
+    if ((movelist[0] == movelist[4] == movelist[8]) or 
+            (movelist[6] == movelist[4] == movelist[2])):
+      win = True
+    if win:
+      update_board(movelist)
+      print("\n")
+      print("You won!".center(WIDTH)) if symbol == "X" else print("You lose!".center(WIDTH))
+      if (symbol == "O"):
+        print("You Have Lost from a Bot Making Random Moves".center(WIDTH))
+        print("You Must Be Ashamed of Yourself".center(WIDTH))
+      print("\n")
+      exit(0)
 
 def display_guide():
   cls()
@@ -60,6 +86,13 @@ def display_board(input_list):
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
+
+def declare_tie(movelist):
+  update_board()
+  print()
+  print("It's a Tie".center(WIDTH))
+  print("\n")
+  exit(0)
 
 if __name__ == "__main__":
   main()
